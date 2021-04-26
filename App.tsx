a5ec68364d0f77e1a3054acce489ccd4f82634ac
gsavchenko <george.savchenko@gmail.com>
2021-04-26 18:16:54 -0400

    moving files to src

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import configureStore from './core/store/store';
-import Login from './login/Login';
+import configureStore from './src/core/store/store';
+import Login from './src/login/Login';
 
 function App() {
   return (
